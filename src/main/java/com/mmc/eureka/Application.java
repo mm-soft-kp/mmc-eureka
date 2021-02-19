1165768475afc3e615b8433a5b5d8da34d41380c
z <z>
2021-02-19 17:15:54 +0800

    init

---
+++ src/main/java/com/mmc/eureka/Application.java
@@ -0,0 +1,19 @@
+package com.mmc.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * TODO
+ *
+ * @author zhiqiang_zhao
+ * @date 2021/2/18 11:35
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
